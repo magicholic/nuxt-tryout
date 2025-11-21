5af7d0b5faa3c40c5e3ff50457870c65fd66f9eb
Fan Gao <fancrazy88@gmail.com>
2025-11-21 13:40:38 -0700

    added routing basic files

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,39 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <div id="first-section">
+      <h1>
+        Hello world!
+      </h1>
+      <firstcomp />
+    </div>
+    <div id="second-section">
+      <nav>
+        <ul>
+          <li><NuxtLink to="/firstpage">First Page</NuxtLink></li>
+          <li><NuxtLink to="/secondpage">Second Page</NuxtLink></li>
+        </ul>
+      </nav>
+    </div>
+    <div>
+      <NuxtPage />
+    </div>
   </div>
 </template>
+
+<style>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin-right: 10px;
+  height: 50px;
+  width: 120px;
+  background-color: azure;
+  line-height: 50px;
+  border-radius: 20px;
+  text-align: center;
+  text-decoration: none;
+}
+</style>
---
+++ app/pages/firstpage.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h2>This is the First Page</h2>
+    </div>
+</template>
